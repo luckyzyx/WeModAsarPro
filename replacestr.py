c3f6b6d85d693315bf72bbd7a1902f861dba66dd
luckyzyx <2813467705@qq.com>
2022-07-10 14:35:06 +0800

    add py

---
+++ replacestr.py
@@ -0,0 +1,16 @@
+
+import json
+dir="unpack/package.json"
+js = json.load(open(dir,'r',encoding='UTF-8'))
+print(js["version"])
+
+str1='if(this.account.subscription){return"pro"}return null}'
+str1fix='if(true){return"pro"}return null}'
+str2='!account.subscription'
+str2fix='false'
+filename="unpack/output/app-bundle.js"
+with open(filename,'r+',encoding='UTF-8') as file:
+	data = file.read()
+	data = data.replace(str1, str1fix)
+	data = data.replace(str2, str2fix)
+	file.write(data)
