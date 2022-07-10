97e2c69e7da3f2d30fcc39c5f2ae1d617a62fe04
luckyzyx <2813467705@qq.com>
2022-07-10 16:53:02 +0800

    fix py

--- replacestr.py
+++ replacestr.py
@@ -4,19 +4,24 @@ dir="unpack/package.json"
 js = json.load(open(dir,'r',encoding='UTF-8'))
 print(js["version"])
 
+# 主页右上角
 str1='if(this.account.subscription){return"pro"}return null'
-str1fix='/*主页右上角*/return"pro"'
-str2='if(!this.account.subscription){this.#qt.open()}break;'
-str2fix='break;'
-str3='#ca(){if(this.theme==="default"||this.subscription){return false}this.#i.dispatch(n.ACTION_SET_SETTINGS,{theme:"default"},"theme_applier",true);return true}'
-str3fix='#ca(){return false}'
-str4='isPro(){return!!this.account?.subscription}'
-str4fix='isPro(){return true}'
-str5='!this.account.subscription'
-str5fix='false'
-str6='!account.subscription'
+str1fix='return"pro"'
+# 一般特殊
+str2='#ca(){if'
+str2fix='#ca(){return false;if'
+str3='isPro(){return!!this.account?.subscription}'
+str3fix='isPro(){return true}'
+# returnfalse 无空格特殊
+str4='case"pro":return!this.account.subscription;'
+str4fix='case"pro":return false;'
+str5='get shouldShowActivatingCheatsCoachingTip(){return!this.account.subscription&&!this.hasUsedHotkeys}'
+str5fix='get shouldShowActivatingCheatsCoachingTip(){return false&&!this.hasUsedHotkeys}'
+# 通用
+str6='!this.account.subscription'
 str6fix='false'
-
+str7='!account.subscription'
+str7fix=' false'
 # copyAppVersion() 复制版本号函数
 
 filename="unpack/output/app-bundle.js"
@@ -28,6 +33,7 @@ with open(filename,'r',encoding='UTF-8') as file:
 	data = data.replace(str4, str4fix)
 	data = data.replace(str5, str5fix)
 	data = data.replace(str6, str6fix)
+	data = data.replace(str7, str7fix)
 with open(filename,'w',encoding='UTF-8') as file:
 	file.write(data)
 	file.close
